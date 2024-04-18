b8c4e9412b100c9e84b62527f3535b6d49c41c9b
ElyCordova <elycordovamiranda@gmail.com>
2024-04-18 08:30:30 -0600

    First commit

---
+++ e-commerce/PrentregaCordova1/src/App.jsx
@@ -0,0 +1,21 @@
+
+
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './Components/NavBar';
+
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <h1>Naturelle</h1>
+        <NavBar/>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
