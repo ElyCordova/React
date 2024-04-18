e67af974645fcdbcc74a036adf96e5ba8693f2b5
ElyCordova <elycordovamiranda@gmail.com>
2024-04-18 09:09:50 -0600

    Writting sections of the NavBar

--- e-commerce/PrentregaCordova1/src/Components/NavBar.jsx
+++ e-commerce/PrentregaCordova1/src/Components/NavBar.jsx
@@ -7,28 +7,28 @@ function NavBar() {
   return (
     <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#naturelle">Naturelle</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
+        <Navbar.Collapse id="responsive-navbar-nav">    
           <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
+          <NavDropdown title="Productos" id="collapsible-nav-dropdown">
+              <NavDropdown.Item href="#aceites">Aceites</NavDropdown.Item>
+              <NavDropdown.Item href="#inciensos">
+                Inciensos
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item href="#jabones">Jabónes</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
+              <NavDropdown.Item href="#tes">
+                Tés
               </NavDropdown.Item>
             </NavDropdown>
+            <Nav.Link href="#blog">Blog</Nav.Link>
+            <Nav.Link href="#contacto">Contacto</Nav.Link>  
           </Nav>
           <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
+            <Nav.Link href="#usuario">icono usuario</Nav.Link>
+            <Nav.Link eventKey={2} href="#carrito">
+              carrito compra
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
